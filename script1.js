5c59276ca6eaff14320e123eb3c9e0455d7356f9
yonasget320-pixel <yonasget320@gmail.com>
2026-01-16 21:10:18 -0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+let x= prompt("please input x");
+x = number(x);
+
+let s =7;
+let isnumber = Number.isInteger(s);
+if(isnumber){
+    console.log("it is anumber");
+}else{
+    console.log("it is not a number");
+
+}
